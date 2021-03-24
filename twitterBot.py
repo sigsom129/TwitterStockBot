02102751ac588326010bcb87ddc2bc6cd6c48fc1
Signory Somsavath <ssomsavath@gmail.com>
2021-03-23 23:57:17 -0500

    Update twitterBot.py

--- twitterBot.py
+++ twitterBot.py
@@ -1,4 +1,6 @@
 import tweepy
+import os 
+import yfinance as yf 
 
 from secrets import user_key, user_secret_key, user_token, user_secret_token
 
@@ -7,4 +9,13 @@ auth = tweepy.OAuthHandler(user_key, user_secret_key)
 auth.set_access_token(user_token, user_secret_token)
 
 api = tweepy.API(auth)
-api.update_status("HELLLLOOOO IS THIS WORKING?")
+# api.update_status("HELLLLOOOO IS THIS WORKING?")
+
+data = yf.download("AMC", period= "1d", interval= "1d")
+print(data)
+
+row = data.iloc[0]
+
+status = "The price of AMC's stock is %s. The close price of AMC's stock is %s. Volume is currently %s" %(
+    str(round(row["Open"],2)), str(round(row["Close"],2)), str(int(row["Volume"]))
+)
