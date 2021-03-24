e102959c9df04abf4a423b439a0775c7abd88393
Signory Somsavath <ssomsavath@gmail.com>
2021-03-23 23:13:59 -0500

    Update twitterBot.py

--- twitterBot.py
+++ twitterBot.py
@@ -1,1 +1,9 @@
 import tweepy
+
+from secrets import user_key, user_secret_key, user_token, user_secret_token
+
+
+auth = tweepy.OAuthHandler(user_key, user_secret_key)
+auth.set_access_token(user_token, user_secret_token)
+
+api = tweepy.API(auth)
